f9b9e3a5c4dc82d26769873ff307bba1af562cf0
Esteban Jenkins <stbnjenkins@gmail.com>
2018-07-31 17:41:30 +0200

    skeleton of app

---
+++ src/application/src/main/java/com/linkedpipes/application/Application.java
@@ -0,0 +1,18 @@
+package com.linkedpipes.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger logger =
+			LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		logger.info("Application started");
+	}
+}
