13209f944fabcbfad3656249202fd548cbd4ebd1
Marzia Cutajar <marzia@ascentsoftware.eu>
2018-08-12 16:15:45 +0200

    Now communicating with discovery service and returning its json response from our api

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/DiscoveryController.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/DiscoveryController.java
@@ -1,26 +1,147 @@
 package com.linkedpipes.lpa.backend;
 
-import com.linkedpipes.lpa.backend.entities.DataSource;
 import com.linkedpipes.lpa.backend.entities.DataSourceList;
 import org.springframework.web.bind.annotation.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.OutputStream;
 
 @RestController
 public class DiscoveryController {
 
     @RequestMapping("/pipelines/discover")
     public Integer startDiscovery(@RequestBody DataSourceList dataSourceList){
-        Integer testPipelineId = 1;
-        return testPipelineId;
+        Integer testDiscoveryId = 1;
+        return testDiscoveryId;
+    }
+
+    @RequestMapping("/pipelines/discoverFromInput")
+    public String startDiscovery(@RequestBody String discoveryConfig){
+        if(discoveryConfig == null || discoveryConfig.isEmpty()) {
+            return "Discovery config not provided";
+        }
+
+        try {
+
+            URL url = new URL("http://demo.visualization.linkedpipes.com:8080/discovery/startFromInput");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+
+            OutputStream os = conn.getOutputStream();
+            os.write(discoveryConfig.getBytes("UTF-8"));
+            os.close();
+
+            conn.connect();
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed: HTTP error code: "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            StringBuilder sb = new StringBuilder();
+            //System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                //System.out.println(output);
+                sb.append(output);
+                sb.append("\n");
+            }
+
+            br.close();
+            conn.disconnect();
+            return sb.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "Error";
     }
 
     @RequestMapping("/discovery/status")
-    public String getDiscoveryStatus(@RequestParam( value="discoveryId") Integer discoveryId){
-        return "Running";
+    public String getDiscoveryStatus(@RequestParam( value="discoveryId") String discoveryId){
+        try {
+            URL url = new URL("http://demo.visualization.linkedpipes.com:8080/discovery/" + discoveryId);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            conn.connect();
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed: HTTP error code: "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            StringBuilder sb = new StringBuilder();
+            //System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                //System.out.println(output);
+                sb.append(output);
+                sb.append("\n");
+            }
+
+            br.close();
+
+            conn.disconnect();
+            return sb.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Error";
     }
 
     @RequestMapping("/discovery/pipelineGroups")
     @ResponseBody
-    public String getPipelineGroups(@RequestParam( value="discoveryId") Integer discoveryId){
+    public String getPipelineGroups(@RequestParam( value="discoveryId") String discoveryId){
+        try {
+            URL url = new URL("http://demo.visualization.linkedpipes.com:8080/discovery/" + discoveryId + "/pipelines");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            conn.connect();
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed: HTTP error code: "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            StringBuilder sb = new StringBuilder();
+            //System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                //System.out.println(output);
+                sb.append(output);
+                sb.append("\n");
+            }
+
+            br.close();
+
+            conn.disconnect();
+            return sb.toString();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return "called /discovery/pipelineGroups";
     }
 
