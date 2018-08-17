06f3fa4d7e9c7614ad5d47fef678fc756c94d6e0
Altynbek Orumbayev <aorumbaev95@gmail.com>
2018-08-17 16:06:34 +0200

    Setup blank React webapp with Webpack

---
+++ src/frontend/src/app.js
@@ -0,0 +1,7 @@
+// import './utils.js'
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+ReactDOM.render(<p>Test</p>, document.getElementById("app"));
