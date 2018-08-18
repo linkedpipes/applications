06895064535190a04fb8a710dc61581ccfff1a91
Marzia Cutajar <marzia@ascentsoftware.eu>
2018-08-18 13:33:28 +0200

    Added new createPipeline endpoint

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/PipelineController.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/PipelineController.java
@@ -1,13 +1,23 @@
 package com.linkedpipes.lpa.backend;
 
 import com.linkedpipes.lpa.backend.entities.Pipeline;
+import com.linkedpipes.lpa.backend.helpers.StreamHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @RestController
 public class PipelineController {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(DiscoveryController.class);
 
     @RequestMapping("/pipeline")
     @ResponseBody
@@ -15,6 +25,35 @@ public class PipelineController {
         Pipeline testPipeline = new Pipeline();
         testPipeline.Uri = "https://discovery.linkedpipes.com/resource/pipeline/1";
         return testPipeline;
+    }
+
+    @RequestMapping("/pipeline/create")
+    @ResponseBody
+    public String createPipeline(@RequestParam( value="discoveryId") String discoveryId, @RequestParam( value="pipelineUri") String pipelineUri){
+        try {
+            URL url = new URL(Application.config.getProperty("discoveryServiceUrl") + "/discovery/" + discoveryId + "/execute/" + pipelineUri);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("Content-Type", "text/plain");
+            conn.setDoOutput(true);
+
+            if (conn.getResponseCode() != 200) {
+                String errorMsg = conn.getResponseCode() + " " + conn.getResponseMessage() + ": "
+                        + StreamHelper.getStringFromStream(conn.getErrorStream());
+                logger.error(errorMsg);
+                return errorMsg;
+            }
+
+            String response = StreamHelper.getStringFromStream(conn.getInputStream());
+            conn.disconnect();
+            return response;
+
+        } catch (IOException e) {
+            logger.error("Exception: ", e);
+        }
+
+        return "An error occurred";
     }
 
     @RequestMapping("/pipeline/execute")
