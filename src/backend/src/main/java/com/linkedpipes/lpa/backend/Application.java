912587f02d64ed2145544a0737ab803fca23035b
Marzia Cutajar <marzia@ascentsoftware.eu>
2018-08-18 13:01:05 +0200

    Added new properties file for global config, added CORS configurer for frontend

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/Application.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/Application.java
@@ -4,12 +4,49 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 @SpringBootApplication
 public class Application {
 
 	private static final Logger logger =
 			LoggerFactory.getLogger(Application.class);
+
+	private static final String NAME = "config.properties";
+
+	public static final Properties config;
+
+	static {
+		File configFile = new File(NAME);
+		config = new Properties();
+
+		try {
+			FileReader reader = new FileReader(configFile);
+			config.load(reader);
+
+			reader.close();
+		} catch (IOException ex) {
+			logger.error("Exception: ", ex);
+		}
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:9000");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
