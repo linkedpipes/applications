b21174cf72bc9523cf784aaf8668f5209eb01d8e
Marzia Cutajar <marzia@ascentsoftware.eu>
2018-09-16 11:22:02 +0200

    Added base.ttl file to use for building ttl from data sources, Added EtlServiceComponent class, now parsing execution status

---
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/entities/EtlStatus.java
@@ -0,0 +1,8 @@
+package com.linkedpipes.lpa.backend.entities;
+
+import com.google.gson.annotations.SerializedName;
+
+public class EtlStatus {
+    @SerializedName("@id")
+    public String id;
+}
--- src/backend/src/main/java/com/linkedpipes/lpa/backend/services/EtlServiceComponent.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/services/EtlServiceComponent.java
@@ -1,4 +1,24 @@
 package com.linkedpipes.lpa.backend.services;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.linkedpipes.lpa.backend.Application;
+import com.linkedpipes.lpa.backend.entities.ExecutionStatus;
+
+import java.io.IOException;
+
 public class EtlServiceComponent {
+    private final HttpUrlConnector httpUrlConnector = new HttpUrlConnector();
+    private final String etlServiceBaseUrl = Application.config.getProperty("etlServiceUrl");
+
+    private String get(String url) throws IOException {
+        return httpUrlConnector.sendGetRequest(url,null, "application/json");
+    }
+    public ExecutionStatus getExecutionStatus(String executionIri) throws IOException {
+        String response = get(executionIri + "/overview");
+        return new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
+                .create().fromJson(response, ExecutionStatus.class);
+    }
 }
