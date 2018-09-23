b21e506d5b98432574bfa650ade38ddfb7851fb2
Marzia Cutajar <marzia@ascentsoftware.eu>
2018-09-23 16:00:07 +0200

    Commented out new StreamUtils class changes due to bug until Ivan makes fix

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/util/StreamUtils.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/util/StreamUtils.java
@@ -1,21 +1,31 @@
 package com.linkedpipes.lpa.backend.util;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 public class StreamUtils {
 
     private static final int BUFFER_SIZE = 10000;
 
+    //TODO Ivan - fix null array exception in below commented code
     /*
      * Closes the stream
      */
     public static String getStringFromStream(InputStream stream) throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        StringBuilder sb;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(stream))) {
+            String output;
+            sb = new StringBuilder();
+            while ((output = br.readLine()) != null) {
+                sb.append(output);
+                sb.append("\n");
+            }
+
+            return  sb.toString();
+
+        /*ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         copyStreamsAndClose(stream, outputStream);
-        return outputStream.toString();
+        return outputStream.toString();*/
+        }
     }
 
     public static void copyStreamsAndClose(InputStream inputStream, OutputStream outputStream) throws IOException {
