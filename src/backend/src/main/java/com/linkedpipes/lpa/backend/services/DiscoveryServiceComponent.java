fdf40d881cff89e1325338da28c3560c07d833ac
Marzia Cutajar <marzia@ascentsoftware.eu>
2018-09-30 10:47:20 +0200

    Revert "Removed unnecessary code, added HashMap mapping ETL Execution Status IRIs"
    This reverts commit 065930de39381aeb69f42638ab5d72d456297c35.

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/services/DiscoveryServiceComponent.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/services/DiscoveryServiceComponent.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 
 public class DiscoveryServiceComponent {
 
+    private final HttpUrlConnector httpUrlConnector = new HttpUrlConnector();
     private final String discoveryServiceBaseUrl = Application.config.getProperty("discoveryServiceUrl");
 
     private String get(String url) throws IOException {
