3939b41c1e3c9e2cbf3b689fefa187801d3ffaa1
Marzia Cutajar <marzia@ascentsoftware.eu>
2018-10-01 12:17:53 +0200

    PipelineGroups endpoint now returns pipelines grouped by app instance

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/entities/DataSource.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/entities/DataSource.java
@@ -4,4 +4,5 @@ public class DataSource {
 
     //TODO use a custom URI object class instead of String type, and use class to automatically validate uri
     public String Uri;
+    public String Label;
 }
