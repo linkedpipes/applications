6a0a6f1be131f5a8a9ee6c4b8b0d42617b5f26cc
Marzia Cutajar <marzia@ascentsoftware.eu>
2018-10-06 11:21:30 +0200

    Now also returning dataSources in /pipelineGroups response

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/entities/DataSource.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/entities/DataSource.java
@@ -1,8 +1,13 @@
 package com.linkedpipes.lpa.backend.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 public class DataSource {
 
     //TODO use a custom URI object class instead of String type, and use class to automatically validate uri
+    @SerializedName("iri")
     public String Uri;
+
+    @SerializedName("label")
     public String Label;
 }
--- src/backend/src/main/java/com/linkedpipes/lpa/backend/entities/PipelineGroup.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/entities/PipelineGroup.java
@@ -6,8 +6,10 @@ import java.util.List;
 public class PipelineGroup {
     public ApplicationInstance applicationInstance;
     public List<Pipeline> pipelines;
+    public List<DataSource> dataSources;
 
     public PipelineGroup(){
         pipelines = new ArrayList<>();
+        dataSources = new ArrayList<>();
     }
 }
