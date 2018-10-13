a539b2b5b1839a88f986a3b071d917b98670558b
Altynbek Orumbayev <aorumbaev95@gmail.com>
2018-10-13 22:53:31 +0200

    Globals actions reducers for global variables in redux

--- src/frontend/src/store.js
+++ src/frontend/src/store.js
@@ -4,10 +4,12 @@ import visualizers from "./_reducers/visualizers";
 import etl_executions from "./_reducers/etl_executions";
 import etl_exports from "./_reducers/etl_exports";
 import logger from "redux-logger";
+import globals from "./_reducers/globals";
 
 export default () => {
   const store = createStore(
     combineReducers({
+      globals: globals,
       datasources: datasourcesReducer,
       visualizers: visualizers,
       etl_executions: etl_executions,
