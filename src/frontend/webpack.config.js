b39122318d7293cb30a100b7c1fa8895e471db0f
Altynbek Orumbayev <aorumbaev95@gmail.com>
2018-10-13 22:52:40 +0200

    Moved index.html to /public; Commands for docker vs local execution

--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -1,55 +1,63 @@
 const path = require("path");
 const webpack = require("webpack");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
-const dev = process.env.NODE_ENV !== "production";
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-  template: path.join(__dirname, "./src/index.html"),
-  filename: "index.html",
-  inject: "body"
-});
+module.exports = () => {
+  const isProduction = process.env.NODE_ENV === "production";
 
-module.exports = {
-  entry: [path.join(__dirname, "/src/index.jsx")],
-  output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "/public"),
-    publicPath: "/"
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.jsx$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  devtool: dev ? "inline-source-map" : "source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "/public"),
-    host: "0.0.0.0",
-    port: "9001",
-    disableHostCheck: true, // solved Invalid-Host-header
-    hot: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
+  let pluginsArray = [
+    new webpack.DefinePlugin({
+      "process.env.BASE_BACKEND_URL": JSON.stringify(
+        process.env.BASE_BACKEND_URL
+      )
+    }),
+    new Dotenv()
+  ];
+
+  if (!isProduction) {
+    pluginsArray.push(new webpack.HotModuleReplacementPlugin());
+  }
+
+  return {
+    entry: [path.join(__dirname, "/src/index.jsx")],
+    output: {
+      filename: "bundle.js",
+      path: path.join(__dirname, "/public", "dist"),
+      publicPath: "/"
     },
-    historyApiFallback: true
-  },
-  mode: dev ? "development" : "production",
-  plugins: dev
-    ? [
-        HTMLWebpackPluginConfig,
-        new webpack.HotModuleReplacementPlugin()
+    resolve: {
+      extensions: [".js", ".jsx"]
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.jsx$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: ["style-loader", "css-loader", "sass-loader"]
+        }
       ]
-    : [HTMLWebpackPluginConfig]
+    },
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "/public"),
+      host: "0.0.0.0",
+      port: "9001",
+      disableHostCheck: true, // solved Invalid-Host-header
+      hot: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      historyApiFallback: true,
+      publicPath: "/dist/"
+    },
+    mode: isProduction ? "production" : "development",
+    plugins: pluginsArray
+  };
 };
