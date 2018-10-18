b581cdd246af3f2e0af470ba3d7eec372cbcf312
Marzia Cutajar <marzia@ascentsoftware.eu>
2018-10-18 18:09:57 +0200

    Now allowing jackson json mapper to ignore case, minor updates

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/entities/PipelineGroup.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/entities/PipelineGroup.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PipelineGroup {
-    public ApplicationInstance visualizers;
+    public ApplicationInstance visualizer;
     public List<DataSourceGroup> dataSourceGroups;
 
     public PipelineGroup(){
