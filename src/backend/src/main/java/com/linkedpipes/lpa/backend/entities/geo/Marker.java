7629a3f60bbbecdf67f4114104eded6a3f656aec
Marzia Cutajar <marzia@ascentsoftware.eu>
2018-10-23 22:42:29 +0200

    Added marker extractor class

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/entities/geo/Marker.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/entities/geo/Marker.java
@@ -3,11 +3,13 @@ package com.linkedpipes.lpa.backend.entities.geo;
 public class Marker {
     public String uri;
     public Coordinate coordinates;
-    public String title;
+    public String label;
     public String description;
 
-    public Marker(String uriIn, Coordinate coordinatesIn){
+    public Marker(String uriIn, Coordinate coordinatesIn, String labelIn, String descriptionIn){
         uri = uriIn;
         coordinates = coordinatesIn;
+        label = labelIn;
+        description = descriptionIn;
     }
 }
---
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/sparql/extractors/geo/MarkerExtractor.java
@@ -0,0 +1,32 @@
+package com.linkedpipes.lpa.backend.sparql.extractors.geo;
+
+import com.linkedpipes.lpa.backend.entities.geo.Coordinate;
+import com.linkedpipes.lpa.backend.entities.geo.Marker;
+import com.linkedpipes.lpa.backend.util.NumberParserUtils;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarkerExtractor {
+
+    public List<Marker> extract(QueryExecution queryExec){
+        List<Marker> markers = new ArrayList<>();
+
+        ResultSet result = queryExec.execSelect();
+
+        while (result.hasNext()) {
+            QuerySolution qs = result.next();
+            markers.add(
+                    new Marker(qs.getResource("s").getURI(),
+                    new Coordinate(NumberParserUtils.tryParseDouble(qs.getLiteral("lat").getString()), NumberParserUtils.tryParseDouble(qs.getLiteral("lng").getString())),
+                            "", ""
+                    ));
+        }
+
+        return markers;
+    }
+}
+
