7f16e25951914ed9e9fba75675ecd0893ffaeb7c
Marzia Cutajar <marzia@ascentsoftware.eu>
2018-10-24 13:37:15 +0200

    Fixed URI passing in MarkerQuery, added method to add filter conditions

---
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/sparql/ValueFilter.java
@@ -0,0 +1,8 @@
+package com.linkedpipes.lpa.backend.sparql;
+
+public class ValueFilter {
+    public String label;
+    public String dataType;
+    public String uri;
+    public Boolean isActive;
+}
--- src/backend/src/main/java/com/linkedpipes/lpa/backend/sparql/queries/geo/MarkerQuery.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/sparql/queries/geo/MarkerQuery.java
@@ -1,10 +1,23 @@
 package com.linkedpipes.lpa.backend.sparql.queries.geo;
 
 import com.linkedpipes.lpa.backend.rdf.vocabulary.SKOS;
+import com.linkedpipes.lpa.backend.sparql.ValueFilter;
+import com.linkedpipes.lpa.backend.sparql.VariableGenerator;
 import com.linkedpipes.lpa.backend.sparql.queries.SparqlQuery;
+import com.linkedpipes.lpa.backend.util.SparqlUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.jena.arq.querybuilder.SelectBuilder;
+import org.apache.jena.sparql.lang.sparql_11.ParseException;
 
+import java.beans.Expression;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//ldvmi: https://github.com/ldvm/LDVMi/blob/master/src/app/model/rdf/sparql/geo/query/MarkerQuery.scala
 public class MarkerQuery implements SparqlQuery {
+
+    Map<String, List<ValueFilter>> filters;
 
     // PREFIXES
     private static final String SKOS_PREFIX = "skos";
@@ -35,12 +48,16 @@ public class MarkerQuery implements SparqlQuery {
     private static final String NAME = SCHEMA_PREFIX + ":name";
     private static final String DESCRIPTION = SCHEMA_PREFIX + ":description";
 
-    public String get(){
-        return new SelectBuilder()
+    public MarkerQuery(Map<String, List<ValueFilter>> filters){
+        this.filters = filters;
+    }
 
-                .addPrefix(SKOS_PREFIX, SKOS_PREFIX_URL)
-                .addPrefix(SCHEMA_PREFIX, SCHEMA_PREFIX_URL)
-                .addPrefix(RDFS_PREFIX, RDFS_PREFIX_URL)
+    public String get() {
+        SelectBuilder builder = new SelectBuilder()
+
+                .addPrefix(SKOS_PREFIX, SparqlUtils.formatUri(SKOS_PREFIX_URL))
+                .addPrefix(SCHEMA_PREFIX, SparqlUtils.formatUri(SCHEMA_PREFIX_URL))
+                .addPrefix(RDFS_PREFIX, SparqlUtils.formatUri(RDFS_PREFIX_URL))
 
                 .addVar(S)
                 .addVar(LAT)
@@ -59,19 +76,47 @@ public class MarkerQuery implements SparqlQuery {
                 .addOptional(S, LABEL, L)
                 .addOptional(S, NOTATION, SN)
                 .addOptional(S, NAME, ST)
-                .addOptional(S, DESCRIPTION, SD)
+                .addOptional(S, DESCRIPTION, SD);
 
-                //TODO also add filter restrictions once filter data is passed to method
+        builder = appendFilterConditions(builder);
 
-                .buildString();
+        return builder.buildString();
     }
 
-    private void getFilterConditions(){
-        //TODO
+    private SelectBuilder appendFilterConditions(SelectBuilder builder) {
+        VariableGenerator varGen = new VariableGenerator();
+        for (Map.Entry<String,List<ValueFilter>> pair : filters.entrySet()){
+            String v = varGen.getVariable();
+            builder.addWhere(S, SparqlUtils.formatUri(pair.getKey()), v);
+
+            for(ValueFilter filter : pair.getValue()) {
+                String labelOrUri = getLabelOrUri(filter);
+                if(StringUtils.isNotEmpty(labelOrUri)) {
+                    try {
+                        builder.addFilter(v + " != " + labelOrUri);
+                    }
+                    catch(ParseException e){
+                        //TODO log exception
+                    }
+                }
+            }
+        }
+        return builder;
+    }
+
+    private String getLabelOrUri(ValueFilter filter){
+        if(StringUtils.isNotEmpty(filter.uri)){
+            return SparqlUtils.formatUri(filter.uri);
+        }
+        else if(StringUtils.isNotEmpty(filter.label)){
+            return SparqlUtils.formatLabel(filter.label);
+        }
+
+        return null;
     }
 
     public static void main(String[] args) {
-        String query = new MarkerQuery().get();
+        String query = new MarkerQuery(new HashMap<>()).get();
         System.out.println("---------------------");
         System.out.println(query);
         System.out.println("---------------------");
