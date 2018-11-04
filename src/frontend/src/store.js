14d5973a776ebea1a10468b7366fe7a497bdba0c
Esteban Jenkins <stbnjenkins@gmail.com>
2018-11-05 00:40:16 +0100

    Fix to the store.js with Redux dev tools

--- src/frontend/src/store.js
+++ src/frontend/src/store.js
@@ -6,6 +6,15 @@ import etl_exports from "./_reducers/etl_exports";
 import logger from "redux-logger";
 import globals from "./_reducers/globals";
 
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(logger));
+
 export default () => {
   const store = createStore(
     combineReducers({
@@ -15,9 +24,7 @@ export default () => {
       etl_executions: etl_executions,
       etl_exports: etl_exports
     }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(logger)
+    enhancer
   );
 
   return store;
