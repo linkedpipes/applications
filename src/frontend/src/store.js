303b86e418f34c17d9c7e9902ff76a9bf06cb6b8
Altynbek Orumbayev <aorumbaev95@gmail.com>
2018-11-05 18:30:11 +0100

    [Frontend][Bug Fix] Imported missing element from redux for dev tools fix

--- src/frontend/src/store.js
+++ src/frontend/src/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore, combineReducers } from "redux";
+import { applyMiddleware, createStore, combineReducers, compose } from "redux";
 import datasourcesReducer from "./_reducers/datasources";
 import visualizers from "./_reducers/visualizers";
 import etl_executions from "./_reducers/etl_executions";
