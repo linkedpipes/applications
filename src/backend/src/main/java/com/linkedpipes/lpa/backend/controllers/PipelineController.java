b7ee53a955f12119a09930fe49c5b57fe2713d2a
Alexandr Mansurov <alex@eghuro.cz>
2018-11-05 23:11:10 +0100

    [Backend][Feature] Service description generator and passing

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/controllers/PipelineController.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/controllers/PipelineController.java
@@ -36,11 +36,23 @@ public class PipelineController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/api/pipeline/export")
-    public ResponseEntity<String> exportPipeline(@RequestParam(value = "discoveryId") String discoveryId, @RequestParam(value = "pipelineUri") String pipelineUri, @RequestBody String serviceDescriptionIri) throws IOException {
-        ServiceDescription serviceDescription = new ServiceDescription(serviceDescriptionIri);
+    @GetMapping("/api/pipeline/exportWithSD")
+    public ResponseEntity<String> exportPipelineWithSD(@RequestParam(value = "discoveryId") String discoveryId, @RequestParam(value = "pipelineUri") String pipelineUri) throws IOException {
+        ServiceDescription serviceDescription = new ServiceDescription("https://gist.githubusercontent.com/eghuro/5d6717d949a01852c2737a8493a3e87a/raw/sd.ttl");
+        //"http://nginx/api/sd?endpoint=db:8890/&namedGraph=http://named.graph/resource/graph/" + discoveryId + "/" + pipelineId);
         String response = discoveryService.exportPipelineUsingSD(discoveryId, pipelineUri, serviceDescription);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/api/sd")
+    public ResponseEntity<String> serviceDescription(@RequestParam(value="endpoint") String endpoint, @RequestParam(value="namedGraph") String namedGraph) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n@prefix ns1: <");
+        sb.append(endpoint);
+        sb.append("> .\n@prefix sd: <http://www.w3.org/ns/sparql-service-description#> .\nns1:sparql rdf:type sd:Service ;\nsd:endpoint ns1:sparql ;\nsd:namedGraph [ sd:name <");
+        sb.append(namedGraph);
+        sb.append("> ];\nsd:feature  sd:DereferencesURIs , sd:UnionDefaultGraph .\n@prefix ns3:    <http://www.w3.org/ns/formats/> .\nns1:sparql  sd:resultFormat ns3:RDFa , ns3:SPARQL_Results_JSON , ns3:SPARQL_Results_XML , ns3:Turtle , ns3:N-Triples , ns3:N3 , ns3:RDF_XML , ns3:SPARQL_Results_CSV ;\nsd:supportedLanguage sd:SPARQL10Query ; sd:url  ns1:sparql .");
+        return ResponseEntity.ok(sb.toString());
     }
 
     @GetMapping("/api/pipeline/create")
