141770c8809a47f2a9f4138b76044316d83f3a41
Altynbek Orumbayev <aorumbaev95@gmail.com>
2018-12-03 23:42:35 +0100

    [Frontend][Refactoring] Fixed typo in markers action

--- src/frontend/src/_actions/markers.js
+++ src/frontend/src/_actions/markers.js
@@ -1,4 +1,4 @@
-export const addMultipleSources = ({ markersList } = {}) => ({
+export const addMultipleMarkers = ({ markersList } = {}) => ({
   type: "ADD_MULTIPLE_MARKERS",
   source: markersList
 });
