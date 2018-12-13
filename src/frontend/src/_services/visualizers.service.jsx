c8144762657e1d7dd3583524fee06268c657cee9
Altynbek Orumbayev <aorumbaev95@gmail.com>
2018-12-13 19:26:31 +0100

    [Frontend][Refactoring] Added SKOSConcepts and SKOSConceptsCount calls

--- src/frontend/src/_services/visualizers.service.jsx
+++ src/frontend/src/_services/visualizers.service.jsx
@@ -1,5 +1,10 @@
 import { BASE_URL, rest } from "./api.service";
 
+const GET_MARKERS_URL = BASE_URL + "map/markers";
+const GET_PROPERTIES_URL = BASE_URL + "map/properties";
+const SKOS_CONCEPTS_URL = BASE_URL + "/api/skos/concepts";
+const SKOS_CONCEPTS_COUNT_URL = BASE_URL + "/api/skos/conceptsCounts";
+
 export const VisualizersService = {
   getFilters: async function() {
     return rest(GET_PROPERTIES_URL, undefined, "GET", undefined);
@@ -12,6 +17,19 @@ export const VisualizersService = {
         getQueryString({ resultGraphIri: resultGraphIri }),
       filters,
       "POST"
+    );
+  },
+
+  getSKOSConcepts: async function() {
+    return rest(SKOS_CONCEPTS_URL, undefined, "GET", undefined);
+  },
+
+  getSKOSConceptsCount: async function({ propertyUri, conceptUris }) {
+    return rest(
+      SKOS_CONCEPTS_COUNT_URL,
+      { propertyUri: propertyUri, conceptUris: conceptUris },
+      "POST",
+      undefined
     );
   }
 };
