3a1b1f0b04149760f23a2a1bcab23b9baca67cc0
Marzia Cutajar <marziacutajar@gmail.com>
2019-03-01 22:07:01 +0100

    [Backend] [Other] Improvement as suggested by Codacy

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/constants/Visualizers.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/constants/Visualizers.java
@@ -4,16 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Visualizers {
-    public static final Map<String, String> map = new HashMap<>(){
-        {
-            put("https://discovery.linkedpipes.com/resource/application/timeline-with-labels/template", "LABELED_TIMELINE");
-            put("https://discovery.linkedpipes.com/resource/application/timeline/template", "TIMELINE");
-            put("https://discovery.linkedpipes.com/resource/application/timeline-periods/template", "TIMELINE_PERIODS");
-            put("https://discovery.linkedpipes.com/resource/application/timeline-periods-with-labels/template", "LABELED_TIMELINE_PERIODS");
-            put("https://discovery.linkedpipes.com/resource/application/dcterms/template", "DCTERMS");
-            put("https://discovery.linkedpipes.com/resource/application/map/template", "MAP");
-            put("https://discovery.linkedpipes.com/resource/application/map-labeled-points/template", "LABELED_POINTS_MAP");
-        }
-    };
+    public static final Map<String, String> map = new HashMap<>();
 
+    static {
+        map.put("https://discovery.linkedpipes.com/resource/application/timeline-with-labels/template", "LABELED_TIMELINE");
+        map.put("https://discovery.linkedpipes.com/resource/application/timeline/template", "TIMELINE");
+        map.put("https://discovery.linkedpipes.com/resource/application/timeline-periods/template", "TIMELINE_PERIODS");
+        map.put("https://discovery.linkedpipes.com/resource/application/timeline-periods-with-labels/template", "LABELED_TIMELINE_PERIODS");
+        map.put("https://discovery.linkedpipes.com/resource/application/dcterms/template", "DCTERMS");
+        map.put("https://discovery.linkedpipes.com/resource/application/map/template", "MAP");
+        map.put("https://discovery.linkedpipes.com/resource/application/map-labeled-points/template", "LABELED_POINTS_MAP");
+    }
 }
