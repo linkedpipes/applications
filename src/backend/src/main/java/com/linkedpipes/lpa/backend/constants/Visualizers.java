7944fc09f7afded9995889ee8bb0acad1ed73f16
Ivan Lattak <ivanlattak@gmail.com>
2019-03-04 21:20:35 +0100

    [Backend] Add Treemap visualizer to the map

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/constants/Visualizers.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/constants/Visualizers.java
@@ -10,6 +10,7 @@ public class Visualizers {
             "https://discovery.linkedpipes.com/resource/application/timeline-periods-with-labels/template", "LABELED_TIMELINE_PERIODS",
             "https://discovery.linkedpipes.com/resource/application/dcterms/template", "DCTERMS",
             "https://discovery.linkedpipes.com/resource/application/map/template", "MAP",
-            "https://discovery.linkedpipes.com/resource/application/map-labeled-points/template", "LABELED_POINTS_MAP");
+            "https://discovery.linkedpipes.com/resource/application/map-labeled-points/template", "LABELED_POINTS_MAP",
+            "https://discovery.linkedpipes.com/resource/application/treemap/template", "TREEMAP");
 
 }
