a6557a6b851f3b9b2cb31b30cbb84a9b92ed4a05
Esteban Jenkins <stbnjenkins@gmail.com>
2019-03-10 17:28:15 +0100

    [Frontend] Google Maps flowized

--- src/frontend/src/components/Visualizers/GoogleMaps/GoogleMapsVisualizer.jsx
+++ src/frontend/src/components/Visualizers/GoogleMaps/GoogleMapsVisualizer.jsx
@@ -1,3 +1,5 @@
+// @flow
+import * as React from 'react';
 import {
   withScriptjs,
   withGoogleMap,
@@ -5,12 +7,85 @@ import {
   Marker
 } from 'react-google-maps';
 import MarkerClusterer from 'react-google-maps/lib/components/addons/MarkerClusterer';
-import React from 'react';
-import { compose, withProps, lifecycle } from 'recompose';
-import { Log } from '@utils';
+import { compose, withProps } from 'recompose';
 import uuid from 'uuid';
+import { Log, VisualizersService } from '@utils';
 
-const GoogleMapsVisualizer = compose(
+type Props = {
+  classes: {
+    progress: number
+  }
+};
+
+type State = {
+  markers: Array<{ coordinates: { lat: number, lon: number } }>,
+  zoomToMarkers: any
+};
+
+class GoogleMapsVisualizer extends React.PureComponent<Props, State> {
+  constructor() {
+    super();
+    this.state = {
+      markers: [],
+      zoomToMarkers: null
+    };
+  }
+
+  async componentDidMount() {
+    const response = await VisualizersService.getMarkers({
+      resultGraphIri: null
+    });
+    const jsonData = await response.json();
+    this.setState({
+      markers: jsonData,
+      zoomToMarkers: map => {
+        const bounds = new window.google.maps.LatLngBounds();
+        if (map !== null) {
+          const childrenArray = map.props.children.props.children;
+          if (childrenArray.length > 0) {
+            map.props.children.props.children.forEach(child => {
+              if (child.type === Marker) {
+                bounds.extend(
+                  new window.google.maps.LatLng(
+                    child.props.position.lat,
+                    child.props.position.lng
+                  )
+                );
+              }
+            });
+            map.fitBounds(bounds);
+          }
+        }
+      }
+    });
+  }
+
+  render() {
+    const { markers, zoomToMarkers } = this.state;
+    return (
+      <GoogleMap
+        ref={zoomToMarkers}
+        defaultZoom={8}
+        defaultCenter={{ lat: 50.08804, lng: 14.42076 }}
+      >
+        <MarkerClusterer averageCenter enableRetinaIcons gridSize={60}>
+          {markers &&
+            markers.length > 0 &&
+            markers.map(marker => (
+              <Marker
+                key={uuid()}
+                position={marker.coordinates}
+                onClick={() => Log.info('Clicked marker')}
+                defaultAnimation={null}
+              />
+            ))}
+        </MarkerClusterer>
+      </GoogleMap>
+    );
+  }
+}
+
+const GoogleMapsVisualizerWithProps = compose(
   withProps({
     googleMapURL:
       'https://maps.googleapis.com/maps/api/js?key=AIzaSyA5rWPSxDEp4ktlEK9IeXECQBtNUvoxybQ&libraries=places',
@@ -18,55 +93,8 @@ const GoogleMapsVisualizer = compose(
     containerElement: <div style={{ height: `100%` }} />,
     mapElement: <div style={{ height: `100%` }} />
   }),
-  lifecycle({
-    componentDidMount() {
-      this.setState({
-        zoomToMarkers: map => {
-          Log.info('Zoomed to markers', 'GoogleMapsVisualizer');
-          const bounds = new window.google.maps.LatLngBounds();
-          if (map !== null) {
-            const childrenArray = map.props.children.props.children;
-            if (childrenArray.length > 0) {
-              map.props.children.props.children.forEach(child => {
-                if (child.type === Marker) {
-                  bounds.extend(
-                    new window.google.maps.LatLng(
-                      child.props.position.lat,
-                      child.props.position.lng
-                    )
-                  );
-                }
-              });
-              map.fitBounds(bounds);
-            }
-          }
-        }
-      });
-    }
-  }),
   withScriptjs,
   withGoogleMap
-)(props => (
-  <GoogleMap
-    ref={props.zoomToMarkers}
-    defaultZoom={8}
-    defaultCenter={{ lat: 50.08804, lng: 14.42076 }}
-  >
-    <MarkerClusterer averageCenter enableRetinaIcons gridSize={60}>
-      {props.markers &&
-        props.markers.length > 0 &&
-        props.markers.map((marker, index) => (
-          <Marker
-            key={uuid()}
-            position={marker.coordinates}
-            onClick={() =>
-              Log.info('Clicked on cluster', 'GoogleMapsVisualizer')
-            }
-            defaultAnimation={null}
-          />
-        ))}
-    </MarkerClusterer>
-  </GoogleMap>
-));
+)(props => <GoogleMapsVisualizer {...props} />);
 
-export default GoogleMapsVisualizer;
+export default GoogleMapsVisualizerWithProps;
