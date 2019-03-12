6cdb7feffd8eb6718f5172d2172c93b09a90ed15
Alexandr Mansurov <alex@eghuro.cz>
2019-03-12 09:04:41 +0100

    [Backend][Fix] Add missing import

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/controllers/PipelineController.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/controllers/PipelineController.java
@@ -18,6 +18,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
