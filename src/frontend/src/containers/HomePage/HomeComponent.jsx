addeb4f84e93f6b8927c1b7ed7514d7e72e52185
Esteban Jenkins <stbnjenkins@gmail.com>
2019-03-24 00:41:33 +0100

    [Frontend] Apps & discoveries table in dashboard (base)

--- src/frontend/src/containers/HomePage/HomeComponent.jsx
+++ src/frontend/src/containers/HomePage/HomeComponent.jsx
@@ -6,6 +6,11 @@ import Typography from '@material-ui/core/Typography/Typography';
 import Button from '@material-ui/core/Button/Button';
 import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import DiscoveriesTableComponent from './children/DiscoveriesTableComponent';
+import ApplicationsTableComponent from './children/ApplicationsTableComponent';
 
 type Props = {
   classes: {
@@ -14,7 +19,13 @@ type Props = {
     button: {},
     templatesBtn: {},
     createBtn: {}
-  }
+  },
+  discoveriesList: Array<{}>,
+  applicationsList: Array<{}>
+};
+
+type State = {
+  tabIndex: number
 };
 
 const styles = theme => ({
@@ -48,52 +59,100 @@ const styles = theme => ({
   }
 });
 
-const HomeComponent = ({ classes }: Props) => (
-  <div className={classes.root}>
-    <Grid container spacing={24}>
-      <Grid item xs={4}>
-        <Paper className={classes.paper}>
-          <Typography variant="subtitle1" gutterBottom>
-            Start by creating a new application
-          </Typography>
-          <Link
-            style={{ textDecoration: 'none', color: 'transparent' }}
-            to="/discover"
-          >
-            <Button
-              variant="contained"
-              size="large"
-              className={classes.createBtn}
-            >
-              Create
-            </Button>
-          </Link>
-          <br />
-          <Button
-            variant="contained"
-            size="large"
-            className={classes.templatesBtn}
-          >
-            Templates
-          </Button>
-        </Paper>
-      </Grid>
-      <Grid item xs={8}>
-        <Paper className={classes.paper}>
-          <Typography variant="subtitle1" gutterBottom>
-            Recent applications
-          </Typography>
-        </Paper>
-      </Grid>
-      <Grid item xs={4}>
-        <Paper className={classes.paper}>
-          <Typography variant="subtitle1" gutterBottom>
-            Running discoveries
-          </Typography>
-        </Paper>
-      </Grid>
-    </Grid>
-  </div>
-);
+class HomeComponent extends React.PureComponent<Props, State> {
+  state = {
+    tabIndex: 0
+  };
+
+  handleChange = (event, tabIndex) => {
+    this.setState({ tabIndex });
+  };
+
+  render() {
+    const { classes, discoveriesList, applicationsList } = this.props;
+    const { tabIndex } = this.state;
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={24}>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>
+              <Typography variant="subtitle1" gutterBottom>
+                Create a new application
+              </Typography>
+              <Link
+                style={{ textDecoration: 'none', color: 'transparent' }}
+                to="/discover"
+              >
+                <Button
+                  variant="contained"
+                  size="large"
+                  className={classes.createBtn}
+                >
+                  Create
+                </Button>
+              </Link>
+              <br />
+              <Typography variant="subtitle1" gutterBottom>
+                Or try one of predefined examples
+              </Typography>
+              <Link
+                style={{ textDecoration: 'none', color: 'transparent' }}
+                to="/discover"
+              >
+                <Button
+                  variant="contained"
+                  size="large"
+                  className={classes.templatesBtn}
+                >
+                  Treemap
+                </Button>
+              </Link>
+              <Link
+                style={{ textDecoration: 'none', color: 'transparent' }}
+                to="/discover"
+              >
+                <Button
+                  variant="contained"
+                  size="large"
+                  className={classes.templatesBtn}
+                >
+                  Google Maps
+                </Button>
+              </Link>
+              <Link
+                style={{ textDecoration: 'none', color: 'transparent' }}
+                to="/discover"
+              >
+                <Button
+                  variant="contained"
+                  size="large"
+                  className={classes.templatesBtn}
+                >
+                  Chord Diagram
+                </Button>
+              </Link>
+            </Paper>
+          </Grid>
+          <Grid item xs={8}>
+            <AppBar position="static" color="secondary">
+              <Tabs value={tabIndex} onChange={this.handleChange} centered>
+                <Tab label="Discoveries" />
+                <Tab label="My Applications" />
+              </Tabs>
+            </AppBar>
+            {tabIndex === 0 && (
+              <DiscoveriesTableComponent discoveriesList={[]} />
+            )}
+            {tabIndex === 1 && (
+              <ApplicationsTableComponent applicationsList={[]} />
+            )}
+            {/* {1 === 0 && <div>Item One</div>}
+        {2 === 1 && <div>Item Two</div>} */}
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 export default withStyles(styles)(HomeComponent);
