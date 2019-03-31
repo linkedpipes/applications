18f07cd20830be0879c1c31882c448cde31dbde0
Alexandr Mansurov <alex@eghuro.cz>
2019-03-31 20:32:55 +0200

    [Database] Update migration script

--- src/backend/src/main/resources/db/migration/V1.1__Extended_user_profile_and_storing_discovery_and_ETL_parameters.sql
+++ src/backend/src/main/resources/db/migration/V1.1__Extended_user_profile_and_storing_discovery_and_ETL_parameters.sql
@@ -1,3 +1,3 @@
 alter table if exists discovery add column if not exists data_sample_iri TEXT, add column if not exists finished timestamp, add column if not exists named_graph TEXT, add column if not exists sparql_endpoint_iri TEXT;
 alter table if exists execution add column if not exists finished timestamp, add column if not exists started timestamp not null;
-create table pipeline_information (id int8 not null, etl_pipeline_iri TEXT not null, pipeline_id uuid not null, result_graph_iri TEXT not null, primary key (id));
+create table pipeline_information (id int8 not null, etl_pipeline_iri TEXT not null, pipeline_id TEXT not null, result_graph_iri TEXT not null, primary key (id));
