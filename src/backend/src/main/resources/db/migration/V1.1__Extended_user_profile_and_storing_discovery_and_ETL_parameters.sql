a471f025f068a11e93e71f4312b81aea7801ba24
Alexandr Mansurov <alex@eghuro.cz>
2019-04-01 10:02:29 +0200

    [Database] Update migration script after merge

--- src/backend/src/main/resources/db/migration/V1.1__Extended_user_profile_and_storing_discovery_and_ETL_parameters.sql
+++ src/backend/src/main/resources/db/migration/V1.1__Extended_user_profile_and_storing_discovery_and_ETL_parameters.sql
@@ -1,3 +1,9 @@
 alter table if exists discovery add column if not exists data_sample_iri TEXT, add column if not exists finished timestamp, add column if not exists named_graph TEXT, add column if not exists sparql_endpoint_iri TEXT;
 alter table if exists execution add column if not exists finished timestamp, add column if not exists started timestamp not null;
 create table pipeline_information (id int8 not null, etl_pipeline_iri TEXT not null, pipeline_id TEXT not null, result_graph_iri TEXT not null, primary key (id));
+alter table if exists lpa_user drop constraint lpa_user_pkey cascade;
+alter table if exists lpa_user drop column id;
+alter table if exists lpa_user add primary key (web_id);
+alter table if exists application alter column user_id type TEXT, add constraint FK99oev92k62cbng2q8vc8pls4o foreign key (user_id) references lpa_user;
+alter table if exists discovery alter column user_id type TEXT, add constraint FKtop5vf25njt7utg0jc1744dnx foreign key (user_id) references lpa_user;
+alter table if exists execution alter column user_id type TEXT, add constraint FKoh7dx8hd2t7gyruje5yb1k9xm foreign key (user_id) references lpa_user;
