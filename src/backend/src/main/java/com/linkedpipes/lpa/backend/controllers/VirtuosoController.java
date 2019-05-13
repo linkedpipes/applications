f5e7398fe307838c571815def59aa4b06e6a5432
Marzia Cutajar <marziacutajar@gmail.com>
2019-05-13 12:14:28 +0200

    [Backend] Added endpoint for checking if named graph exists in Virtuoso

--- src/backend/src/main/java/com/linkedpipes/lpa/backend/controllers/VirtuosoController.java
+++ src/backend/src/main/java/com/linkedpipes/lpa/backend/controllers/VirtuosoController.java
@@ -1,6 +1,11 @@
 package com.linkedpipes.lpa.backend.controllers;
 
+import com.linkedpipes.lpa.backend.exceptions.LpAppsException;
 import com.linkedpipes.lpa.backend.services.TtlGenerator;
+import com.linkedpipes.lpa.backend.services.virtuoso.VirtuosoService;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +17,12 @@ public class VirtuosoController {
     public static final String GRAPH_NAME_PREFIX = "https://applications.linkedpipes.com/graph/";
     private static final String SERVICE_DESCRIPTION_PATH = "/api/virtuoso/serviceDescription";
 
+    @NotNull private final VirtuosoService virtuosoService;
+
+    public VirtuosoController(ApplicationContext context){
+        virtuosoService = context.getBean(VirtuosoService.class);
+    }
+
     /**
      * Get service description of our virtuoso SPARQL endpoint
      * @param graphId
@@ -20,5 +31,14 @@ public class VirtuosoController {
     @GetMapping(SERVICE_DESCRIPTION_PATH)
     public ResponseEntity<String> serviceDescription(@RequestParam(value = "graphId") String graphId) {
         return ResponseEntity.ok(TtlGenerator.getVirtuosoServiceDescription(GRAPH_NAME_PREFIX + graphId));
+    }
+
+    @GetMapping("api/virtuoso/graphExists")
+    public ResponseEntity checkNamedGraphExists(@RequestParam(value = "graphId") String graphId) throws LpAppsException {
+        boolean exists = virtuosoService.checkNamedGraphExists(graphId);
+        if (exists)
+            return ResponseEntity.ok("");
+
+        throw new LpAppsException(HttpStatus.NOT_FOUND, "Named graph does not exist.");
     }
 }
