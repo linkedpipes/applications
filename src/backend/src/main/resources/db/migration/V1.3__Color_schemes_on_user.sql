9580282c6b3882aa655c2d8bfa6af7143bf4fec4
Alexandr Mansurov <alex@eghuro.cz>
2019-05-19 18:46:43 +0200

    [Database] Merge new migrations into one

--- src/backend/src/main/resources/db/migration/V1.3__Color_schemes_on_user.sql
+++
@@ -1,3 +0,0 @@
-alter table if exists lpa_user add column if not exists color VARCHAR(255);
-
-update lpa_user set color = 'BLACK';
--- src/backend/src/main/resources/db/migration/V1.5__Named_graphs_on_delete_cascade.sql
+++
@@ -1,6 +0,0 @@
-alter table if exists discovery_named_graph
-    drop constraint FKy765fuyjbstexc,
-    add constraint FKy765fuyjbstexc
-        foreign key (discovery_id)
-        references discovery
-        on delete cascade;
