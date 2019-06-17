cd6319940effde291f34bb57b0f13fe58d0400b1
Altynbek Orumbayev <aorumbayev@protonmail.com>
2019-06-18 00:57:19 +0200

    [Frontend] ESlint fixes

--- src/frontend/src/components/Visualizers/Treemap/TreemapVisualizer.jsx
+++ src/frontend/src/components/Visualizers/Treemap/TreemapVisualizer.jsx
@@ -4,7 +4,6 @@ import Chart from 'react-google-charts';
 import { withStyles } from '@material-ui/core/styles';
 import { VisualizersService } from '@utils';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import uuid from 'uuid';
 import Button from '@material-ui/core/Button';
 
 type Props = {
