575192db507cfa3f1d98814b1284f510fd8721eb
Altynbek Orumbayev <aorumbayev@protonmail.com>
2019-07-05 15:57:44 +0200

    [Frontend] ESLint fixes

--- src/frontend/src/components/Header/AboutHeader/AboutHeaderComponent.jsx
+++ src/frontend/src/components/Header/AboutHeader/AboutHeaderComponent.jsx
@@ -1,14 +1,8 @@
 // @flow
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import HelpIcon from '@material-ui/icons/Help';
-import IconButton from '@material-ui/core/IconButton';
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs';
 import Toolbar from '@material-ui/core/Toolbar';
-import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
