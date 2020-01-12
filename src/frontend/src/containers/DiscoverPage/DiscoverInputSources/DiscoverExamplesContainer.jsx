72e310e44e22f19208239d286efe0e5e3bcd1662
Altynbek Orumbayev <aorumbaev95@gmail.com>
2020-01-12 01:03:48 +0100

    Removing broken Maps template

--- src/frontend/src/containers/DiscoverPage/DiscoverInputSources/DiscoverExamplesContainer.jsx
+++ src/frontend/src/containers/DiscoverPage/DiscoverInputSources/DiscoverExamplesContainer.jsx
@@ -18,19 +18,19 @@ export const samples = [
     dataSampleIri:
       'https://gist.githubusercontent.com/ivan-lattak/63801f3e6a9e6105aada4c207d0f8abb/raw/aa2a805a4b14da6fd30711532c4f58692018665b/cpv-2008_sample.ttl'
   },
-  {
-    id: uuid.v4(),
-    inputType: 'SPARQL_ENDPOINT',
-    label: 'Maps Sample',
-    type: 'MAPS',
-    description:
-      'This is an example of Maps visualizer. Using this template will produce a sample Application displaying markers on a Map visualizer.',
-    sparqlEndpointIri: 'https://lpatest.opendata.cz/sparql',
-    backgroundColor: '#2d7c9d',
-    namedGraph: 'https://lpatest.opendata.cz/graphs/ruian-test-buildings',
-    dataSampleIri:
-      'https://gist.githubusercontent.com/Ponsietta/1cfcc9cc018c11e39afe1553d0b3f25f/raw/266634bcc2b5bb56b5eed8dbcaf69df9f719cf08/ruian-test-buildings_map_sample.ttl'
-  },
+  // {
+  //   id: uuid.v4(),
+  //   inputType: 'SPARQL_ENDPOINT',
+  //   label: 'Maps Sample',
+  //   type: 'MAPS',
+  //   description:
+  //     'This is an example of Maps visualizer. Using this template will produce a sample Application displaying markers on a Map visualizer.',
+  //   sparqlEndpointIri: 'https://lpatest.opendata.cz/sparql',
+  //   backgroundColor: '#2d7c9d',
+  //   namedGraph: 'https://lpatest.opendata.cz/graphs/ruian-test-buildings',
+  //   dataSampleIri:
+  //     'https://gist.githubusercontent.com/Ponsietta/1cfcc9cc018c11e39afe1553d0b3f25f/raw/266634bcc2b5bb56b5eed8dbcaf69df9f719cf08/ruian-test-buildings_map_sample.ttl'
+  // },
   {
     id: uuid.v4(),
     inputType: 'SPARQL_ENDPOINT',
